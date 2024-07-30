ed13de41d79bac8d7b3e26e5e197f819e4b17224
N0creeper <noam@tindav.fr>
2024-07-30 14:54:07 +0200

    0.1

--- friendcount.py
+++ friendcount.py
@@ -1,18 +1,28 @@
 Nom1, Nom2= 'NON DEFINI1','NON DEFINI2'
 argent1, argent2=0,0
 
-print('Bienvenue sur tricount express pour 2 personnes')
+print('Bienvenue sur Freidnscount 0.1')
+print('')
+
 while True:
-    print('Commands:\/recap --> Informations\/set people name --> Modifier les noms des personnes\/add money --> ajouter de l"argent ')
+    print('Commands:')
+    print('/recap --> Informations')
+    print('/set people name --> Modifier les noms des personnes')
+    print('/add money --> ajouter de l"argent')
+    print('/remove money --> retirer de l"argent')
+    print('')
+    
     commande=(input("Command: "))
     if commande == '/recap':
         print(Nom1,'a payé',argent1)
         print(Nom2,'a payé',argent2)
+        print('')
     #elif commande == '/set people number': pour plus tard
 
     elif commande == '/set people name':
         Nom1 = (input('people 1 name: '))
         Nom2 = (input('people 2 name: '))
+        print('')
         
     elif commande == '/add money':
         moneyadd=(input('Qui: '))
@@ -23,6 +33,7 @@ while True:
         else:
             print('ERROR : not a name')
         moneyadd=0
+        print('')
             
     elif commande == '/remove money':
         moneyremove=(input('Qui: '))
@@ -31,10 +42,13 @@ while True:
         elif moneyremove == Nom2:
             argent2 = argent2 - int(input('ajouter combien? : ')) 
         else:
-            print('ERROR : not a name')
+            print('Error : not a name')
         moneyremove=0
-
+        print('')
+        
     #elif commande == '/calculate':
 
     else:
-        print('not a command')
+        print('Error: not a command')
+        print('')
+
